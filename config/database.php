2b863356a73a1dc840c083cfd3dea5e906ccf1a0
TmihiW <kadirfindik3871@gmail.com>
2022-11-23 12:16:53 +0300

    Initial commit

--- config/database.php
+++ config/database.php
@@ -14,7 +14,7 @@ return [
     | you may use many connections at once using the Database library.
     |
     */
-
+    //define database connection here
     'default' => env('DB_CONNECTION', 'mysql'),
 
     /*
