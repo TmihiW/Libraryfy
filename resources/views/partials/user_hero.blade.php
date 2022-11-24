4d09e06425de686cdb52f4d38f31064a85013419
TmihiW <kadirfindik3871@gmail.com>
2022-11-24 21:40:37 +0300

    Initial commit

--- resources/views/partials/user_hero.blade.php
+++ resources/views/partials/user_hero.blade.php
@@ -8,16 +8,16 @@
 
         <div class="z-10">
             <h1 class="text-6xl font-bold uppercase text-white">
-                LARA<span class="text-black">GIGS</span>
+                Library<span class="text-black">fy</span>
             </h1>
             <p class="text-2xl text-gray-200 font-bold my-4">
-                Find or post Laravel jobs & projects
+                Rent Books 
             </p>
             <div>
                 <a
                     href="register.html"
                     class="inline-block border-2 border-white text-white py-2 px-4 rounded-xl uppercase mt-2 hover:text-black hover:border-black"
-                    >Sign Up to List a gig</a
+                    >Sign Up to Rent a Book</a
                 >
             </div>
         </div>
