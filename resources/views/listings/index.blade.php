40821e92f756bddd32951d20fc8fc3983e1c64ba
TmihiW <kadirfindik3871@gmail.com>
2022-11-25 15:26:41 +0300

    Initial commit

--- resources/views/listings/index.blade.php
+++ resources/views/listings/index.blade.php
@@ -1,19 +1,23 @@
 <x-layout>
-@include('partials._hero')
-@include('partials._search')
+    @include('partials._hero')
+    @include('partials._search')
 
 <!-- syntax without php in blade template -->
 <!-- similar to jsx (react), angular -->
 {{--<h1>{{$heading}}</h1>--}}
 
-<div class="lg:grid lg:grid-cols-2 gap-4 space-y-4 md:space-y-0 mx-4">
+    <div class="lg:grid lg:grid-cols-2 gap-4 space-y-4 md:space-y-0 mx-4">
 
-    @unless(count($listingsValues)==0)
-        @foreach($listingsValues as $listing) 
-            <x-listing-card :listing="$listing" />
-        @endforeach
-    @else
-        <p>No listings found</p>
-    @endunless
-</div>
+        @unless(count($listingsValues)==0)
+            @foreach($listingsValues as $listing) 
+                <x-listing-card :listing="$listing" />
+            @endforeach
+        @else
+            <p>No listings found</p>
+        @endunless
+    </div>
+    {{-- .mt-6.p4 --}}
+    <div class="mt-6 p4">
+        {{$listingsValues->links()}}
+    </div>
 </x-layout>
