75fe64404404ee8d1f6d0e1ff1b5a433e94bb85c
TmihiW <kadirfindik3871@gmail.com>
2022-11-29 18:09:06 +0300

    Initial Commit

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -8,4 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     use HasFactory;
+    protected $table = 'category';
+    protected $primaryKey = 'c_id';
+
+    public function scopeFilter($query,array $filters){        
+        if($filters['search'] ?? false){
+            $query->when($filters['search'],function($query,$search){
+                $query->where('c_name_','like','%'.$search.'%');
+            });       
+        }    
+    }
+    //Relationship with BookCategory
+    public function book_category(){
+        return $this->hasMany(BookCategory::class,'id_category');
+    }
 }
